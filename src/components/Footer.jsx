3c45189410752a5445a92c6986bf9ccfc4e5a307
Mayra Malaquias <mayra.malaquias@hotmail.com>
2023-05-07 23:28:29 -0300

    Style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
-import styles from '../styles/footer.module.css'
+import styles from "../styles/footer.module.css";
 
 export function Footer() {
   return (
     <div className={styles.footerContainer}>
-      <p className={styles.footerText}>Feito com ♡ por Simara Conceição para PrograMaria.</p>
+      <p className={styles.footerText}>
+        Feito com ♡ por Mayra Malaquias No Curso Programaria.
+      </p>
     </div>
-  )
+  );
 }
